05474c90952a92f5ff3e72e6d1b9bec5a4030f7d
Miguel Quezada <mig.aquezada@gmail.com>
2022-10-23 00:44:22 -0300

    adds tableview and cell

---
+++ TwitterClone/Controllers/Core/DirectMessagesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DirectMessagesViewController.swift
+//  TwitterClone
+//
+//  Created by Miguel Quezada on 23-10-22.
+//
+
+import Combine
+import UIKit
+
+class DirectMessagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
